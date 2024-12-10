da906c346ad01e77e34a60d1a3c7087cc09e762a
EanCloma <meryanne.cloma@carsu.edu.ph>
2024-12-10 19:02:00 +0800

    Add files via upload

---
+++ src/script.js
@@ -0,0 +1,5 @@
+import Slider from "./Main/Slider";
+import Main from "./Main/Main";
+
+const main = new Main(document.querySelector("canvas.webgl"));
+const slider = new Slider(document.querySelector('.range [type="range"]'));
